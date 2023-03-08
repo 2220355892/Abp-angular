9c0d78c5ba7928c01d1b10304150f89a93a5fc93
Nhật Võ <vohoangnhatvip1@gmail.com>
2023-03-08 16:24:09 +0700

    code ne

---
+++ aspnet-core/TodoApp/Entities/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace TodoApp.Entities
+{
+    public class TodoItem : BasicAggregateRoot<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+    }
+}
